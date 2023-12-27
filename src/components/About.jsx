04a4bf979b1698859e2ae5e3a3e6a33b18be559a
ayushshresth021 <ayush.shresth17@gmail.com>
2023-12-27 13:35:50 -0800

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ import {fadeIn, textVariant } from '../utils/motion';
 const ServiceCard = ({index, title, icon}) => {
   return(
     <p> 
-      <Tilt className="xs:w[250px] w-full"> 
+      <Tilt className="xs:w[250px] w-[230px]"> 
         <motion.div
           variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
           className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-cardgit "
@@ -23,7 +23,7 @@ const ServiceCard = ({index, title, icon}) => {
               speed: 450
             }}
             className="bg-tertiary rounded-[20px] py-5 px-12 
-            min-h-[280px] flex justify-evenly items-center flex-col"
+            min-h-[250px] flex justify-evenly items-center flex-col"
             >
               <img src={icon} alt={title} className="w-16 h-16 object-contain" />
               <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
@@ -45,7 +45,6 @@ const About = () => {
         variants={fadeIn("","",0.1,1)}
         className="mt-4 text-secondary text-[17px] max-w-3x1 leading-[30px]"
       >
-        Write a description here
       </motion.p> 
       <div className='mt-20 flex flex-wrap gap-10 w-full h-full'>
         {services.map((service, index) => (<ServiceCard key={service.title} 
